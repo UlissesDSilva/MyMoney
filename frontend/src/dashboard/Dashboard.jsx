edfa5ab40ca7276443206d4c8ffe09b337e92b68
Ulisses Queiroz D. Silva <uqs.dsilva@gmail.com>
2021-06-17 20:58:32 -0300

    Layout dashboard

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import Content from '../common/template/Content'
 import ContentHeader from '../common/template/ContentHeader'
+import ValueBox from '../common/widget/ValueBox'
+import Row from '../common/layout/Row'
 
 class Dashboard extends Component{
     render(){
@@ -9,7 +11,11 @@ class Dashboard extends Component{
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'/>
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de Crédito'/>
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos'/>
+                        <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor Consolidado'/>
+                    </Row>    
                 </Content>
             </div>
         )
