90892b151080271a65e94823a87cb0273ce7454b
Ulisses Queiroz D. Silva <uqs.dsilva@gmail.com>
2021-08-10 00:57:38 -0300

    Atutenticação

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,18 +1,29 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server){
 
-    //URL base para todas as rotas
-    const router = express.Router()
-    
-    //Toda url que comece com "/api" será direciona para o middleware 'router'
-    server.use('/api', router)
+    // Proteger as rotas que comecem com '/api' - Rotas protegidas pelo jwt
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    // Middleware de autenticação é aplicado para todos que forem contemplados pelo 'protected'
+    protectedApi.use(auth)
 
     //Mapeamento das rotas de Ciclo de Pagamento
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    // Registra em 'router'(nas rotas) todos os webServices rest definidos em 'billingCycleService' dentro de '/billingCycle'
-    BillingCycle.register(router, '/billingCycles')
+    // Registra em 'protectedApi'(nas rotas) todos os webServices rest definidos em 'billingCycleService' dentro de '/billingCycle'
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    // Rotas abertas
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
 
-// Ao acessar '/api' será direcionado para o middleware 'router'
-// No 'router' BillingCycle será mapeado para '/billingCycles'
+// Ao acessar '/api' será direcionado para o middleware 'protectedApi'
+// No 'protectedApi' BillingCycle será mapeado para '/billingCycles' e estará protegido
